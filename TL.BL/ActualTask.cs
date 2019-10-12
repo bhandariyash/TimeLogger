3a84577dfb941a33ca4f382e6e318dc558bde8e6
YASH BHANDARI <bhandariyash94@gmail.com>
2019-10-13 04:00:24 +0530

    Added StartTime, EndTime and TaskDuration properties to AcutalTask class

--- TL.BL/ActualTask.cs
+++ TL.BL/ActualTask.cs
@@ -12,6 +12,40 @@ namespace TL.BL
         public int ActualTaskUserId { get; private set; }
         public string Location { get; set; }
         public string[] ActualTaskParticipants { get; set; }
-        public int MyProperty { get; set; }
+
+        private DateTime _startDateTime;
+        public DateTime StartDateTime
+        {
+            get
+            {
+                //Make sure to convert UTC date time to client machine's timezone's datetime/configured timezone's datetime
+                return _startDateTime;
+            }
+
+            set
+            {
+                //Convert client machine's timezone's local datetime/configured timezone's datetime to UTC datetime
+                _startDateTime = value;
+            }
+        }
+
+        private DateTime _endDateTime;
+        public DateTime EndDateTime
+        {
+            get
+            {
+                //Make sure to convert UTC date time to client machine's timezone's datetime/configured timezone's datetime
+                return _endDateTime;
+            }
+
+            set
+            {
+                //Convert client machine's timezone's local datetime/configured timezone's datetime to UTC datetime
+                _endDateTime = value;
+            }
+        }
+
+        public TimeSpan TaskDuration { get; set; }
+
     }
 }
